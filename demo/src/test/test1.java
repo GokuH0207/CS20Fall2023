3a3281582db4eb6e1810a1407ea6942e7442382c
37033005 <37033005@L815WD220022.edu.cbe.ab.ca>
2023-09-12 09:43:31 -0600

    9:43am

---
+++ demo/src/test/test1.java
@@ -0,0 +1,24 @@
+package test;
+
+class test1 {
+
+	public static void main(String[] args) {
+		
+		//declaration
+		int age;
+		double cost;
+		String name;
+		char letter;
+		
+		//initialization
+		age = 16;
+		cost = 45.5;
+		name = "Jeff";
+		letter = 'B';
+		System.out.print(age+cost+ name);
+				
+		//System.out.print("Hello");
+
+	}
+
+}
