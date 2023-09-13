aa4dd59deaee9d24e04ee7982345105f2620f19a
37033005 <37033005@L815WD220022.edu.cbe.ab.ca>
2023-09-13 11:06:52 -0600

    11:06am

--- demo/src/demo/Demo1.java
+++ demo/src/demo/Demo1.java
@@ -1,13 +1,24 @@
 package demo;
 
+import java.util.Scanner;
+
 public class Demo1 {
 
 	public static void main(String[] args) {
 		
-		System.out.print("HELLLOO ARTIN");
+	//	System.out.print("HELLLOO ARTIN");
 		
-		System.out.print("hello");
 		
+		 
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("PLease enter the first number: ");
+		int num1 = input.nextInt();
+		System.out.print("PLease enter the second number: ");
+		int num2 = input.nextInt();
+		int sum = num1*num2;
+		System.out.print("After deviding the 2 numbers the answer is: " + sum);
 	}
 
 }
+     
